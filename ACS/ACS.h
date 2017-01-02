326a34b692d7f4009ec8de65e749815406e5a165
Guilherme Rambo <insidegui@gmail.com>
2017-01-02 20:57:49 -0200

    Moved AssetCatalogReader to a separate framework

---
+++ ACS/ACS.h
@@ -0,0 +1,20 @@
+//
+//  ACS.h
+//  ACS
+//
+//  Created by Guilherme Rambo on 02/01/17.
+//  Copyright © 2017 Guilherme Rambo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ACS.
+FOUNDATION_EXPORT double ACSVersionNumber;
+
+//! Project version string for ACS.
+FOUNDATION_EXPORT const unsigned char ACSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACS/PublicHeader.h>
+
+
+#import <ACS/AssetCatalogReader.h>
--- Asset Catalog Tinkerer/AssetCatalogDocument.swift
+++ Asset Catalog Tinkerer/AssetCatalogDocument.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import ACS
 
 class AssetCatalogDocument: NSDocument {
 

8d07a7871600e4b4f5b7747d52fdb6dc9aa421fe
falchion <122236745+falchion10@users.noreply.github.com>
2025-07-17 22:54:03 -0500

    Close app when no windows
    Automatically close the app when no windows are open.

--- Asset Catalog Tinkerer/AppDelegate.swift
+++ Asset Catalog Tinkerer/AppDelegate.swift
@@ -25,7 +25,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
 }
 
